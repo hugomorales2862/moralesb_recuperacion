346c51b4b2db997ccbb188c2a3de4b6274ed39c9
Hugo Morales <moralesbatzh@gmail.com>
2023-07-06 09:38:23 -0600

    se creo el include navbar

---
+++ includes/navbar.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#"></a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio<i class="ion-ios-mic-off"></i></a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Viviendas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/moralesb_recuperacion/vistas/vivienda/index.php">Registrar Viviendas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/moralesb_recuperacion/vistas/vivienda/buscar.php">Buscar</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Visitas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/moralesb_recuperacion/vistas/visita/index.php">Registrar Visita</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/moralesb_recuperacion/vistas/visita/buscar.php">Buscar</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Infmoracion de las visitas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="/moralesb_recuperacion/vistas/detalle/buscar.php">Ver detalle de Visitas</a>
+                        </li>
+                    </ul>
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+</nav>
