9ca1c0207ae94782ee581f116be58a4b1b71bfa3
Hugo Morales <moralesbatzh@gmail.com>
2023-07-06 12:38:05 -0600

    correcciones

--- vistas/visita/index.php
+++ vistas/visita/index.php
@@ -3,50 +3,31 @@
 <div class="container mt-5">
   <h1 class="text-center mt-3">Registro de Visitas</h1>
   <div class="row justify-content-center mt-2">
-    <div class="col-lg-8">
-      <form action="/moralesb_recuperacion/controladores/visita/guardar.php" method="POST" class="border bg-light p-3">
-        <div class="row mb-3">
+    <form action="/moralesb_recuperacion/controladores/visita/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+      <div class="row mb-3">
+        <div class="col">
+          <label for="nombre" class="form-label">Ingrese el Nombre de la Persona Visitante</label>
+          <input type="text" name="nombre" id="nombre" class="form-control" required>
           <div class="col">
-            <label for="nombre" class="form-label">Ingrese el Nombre de la Persona Visitante</label>
-            <input type="text" name="nombre" id="nombre" class="form-control" required>
-          </div>
-          <div class="col">
-            <label for="dpi" class="form-label">Ingrese el Numero de DPI</label>
-            <input type="number" name="dpi" id="dpi" class="form-control" required>
-          </div>
-          <div class="col">
-            <label for="hora_ingreso" class="form-label">Ingrese la Hora de ingreso</label>
-            <input type="time" name="hora_ingreso" id="hora_ingreso" class="form-control" required>
-          </div>
-          <div class="col">
-            <label for="hora_salida" class="form-label">Ingrese la Hora de salida</label>
-            <input type="time" name="hora_salida" id="hora_salida" class="form-control" required>
-          </div>
+          <label for="dpi" class="form-label">Ingrese el Numero de DPI</label>
+          <input type="number" name="dpi" id="dpi" class="form-control" required>
         </div>
-        <div class="row mb-3">
-          <div class="col">
-            <button type="submit" class="btn btn-primary w-100">Guardar</button>
-          </div>
+        <div class="col">
+          <label for="hora_ingreso" class="form-label">Ingrese la Hora de ingreso</label>
+          <input type="datetime-local" name="hora_ingreso" id="hora_ingreso" class="form-control" required>
         </div>
-      </form>
-    </div>
-  </div>
-  <div class="row justify-content-center mt-2">
-    <div class="col-lg-8">
-      <form action="/moralesb_recuperacion/controladores/detalle/guardar.php" method="POST" class="border bg-light p-3">
-        <div class="row mb-3">
-          <div class="col">
-            <label for="fecha" class="form-label">Ingrese la fecha</label>
-            <input type="date" name="fecha" id="fecha" class="form-control" required>
-          </div>
+        <div class="col">
+          <label for="hora_salida" class="form-label">Ingrese la Hora de salida</label>
+          <input type="datetime-local" name="hora_salida" id="hora_salida" class="form-control" required>
         </div>
-        <div class="row mb-3">
-          <div class="col">
-            <button type="submit" class="btn btn-primary w-100">Guardar</button>
-          </div>
         </div>
-      </form>
-    </div>
+      </div>
+      <div class="row mb-3">
+        <div class="col">
+          <button type="submit" class="btn btn-primary w-100">Guardar</button>
+        </div>
+      </div>
+    </form>
   </div>
 </div>
 <?php include_once '../../includes/footer.php'?>
---
+++ vistas/visita/modificar.php
@@ -0,0 +1,52 @@
+<?php
+require_once '../../modelos/Visita.php';
+try {
+
+    $visita = new Visita($_GET);
+
+    $visitas = $visita->buscar();
+    //var_dump($visitas);
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
+?>
+<?php include_once '../../includes/header.php' ?>
+<div class="container">
+    <h1 class="text-center">Modificar Registro de visitas</h1>
+    <div class="row justify-content-center">
+        <form action="/moralesb_recuperacion/controladores/visita/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <input type="hidden" name="ID" value="<?= $visitas[0]['ID'] ?>">
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="nombre">nombre</label>
+                    <input type="text" name="nombre" id="nombre" value="<?= $visitas[0]['NOMBRE_CONDOMINIO'] ?>" class="form-control" required>
+                </div>
+                <div class="col">
+                    <label for="dpi">Numero de DPI</label>
+                    <input type="number" name="dpi" id="dpi" value="<?= $visitas[0]['NUMERO_visita'] ?>" class="form-control" required>
+
+                </div>
+                <div class="col">
+                    <label for="hora_ingreso">Hora de ingreso </label>
+                    <input type="text" name="hora_ingreso" id="hora_ingreso" value="<?= $visitas[0]['propietaria'] ?>" class="form-control" required>
+
+                </div>
+                <div class="col">
+                    <label for="hora_salida">Hora de salida</label>
+                    <input type="text" name="hora_salida" id="hora_salida" value="<?= $visitas[0]['propietaria'] ?>" class="form-control" required>
+
+                </div>
+              
+            </div>
+
+            <div class="row mb-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-warning w-100">Modificar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<?php include_once '../../includes/footer.php' ?>
