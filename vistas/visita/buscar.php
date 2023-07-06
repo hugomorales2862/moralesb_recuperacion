ba8a1581113ece1b5a3e5ac8e5aa1c0daebcb64c
Hugo Morales <moralesbatzh@gmail.com>
2023-07-06 11:22:33 -0600

    se creo la vista buscar para la clase visita

---
+++ vistas/visita/buscar.php
@@ -0,0 +1,22 @@
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Buscar Visitas</h1>
+        <div class="row justify-content-center">
+            <form action="/moralesb_recuperacion/controladores/visita/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="nombre">Nombre del visitante</label>
+                        <input type="text" name="nombre" id="nombre" class="form-control" >
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-info w-100">Buscar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php include_once '../../includes/footer.php'?>
